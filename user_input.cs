951c6a4dfc5919fb972188aad5a76f7477ab9d0d
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2018-07-10 21:07:50 +0530

    Taking user input

---
+++ user_input.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Program
+{
+class user_input
+{
+  public static void Main(String[] args)
+  {
+    String user="";
+    Console.WriteLine("Enter a word");
+    user=Console.ReadLine();
+    Console.WriteLine("You have entered: "+user);
+  }
+}
+}
